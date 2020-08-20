d643bbf1180325da61b425f164e13d993106303a
Tom Rix <trix@redhat.com>
2020-08-20 14:37:08 -0400

    Rework dkms driver to dfl codebase
    Signed-off-by: Tom Rix <trix@redhat.com>

--- dkms-postrem.sh
+++ dkms-postrem.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
-if [ -f $DESTDIR/etc/udev/rules.d/40-intel-fpga.rules ]; then
-  rm -f $DESTDIR/etc/udev/rules.d/40-intel-fpga.rules
+if [ -f $DESTDIR/etc/udev/rules.d/40-dfl-fpga.rules ]; then
+  rm -f $DESTDIR/etc/udev/rules.d/40-dfl-fpga.rules
 fi
 
-rmmod i2c-altera \
-	  intel-generic-qspi \
-	  intel-on-chip-flash \
-	  altera-asmip2 \
-	  spi-altera-mod \
-	  spi-nor-mod \
-	  intel-max10 \
-	  avmmi-bmc \
-	  intel-fpga-fme \
-	  fpga-mgr-mod \
-	  intel-fpga-afu \
-	  intel-fpga-pci \
-	  intel-fpga-pac-hssi \
-	  intel-fpga-pac-iopll \
-	  c827_retimer \
-	  pac_n3000_net \
-	  ifpga-sec-mgr \
-	  spi-bitbang-mod \
-	  regmap-mmio-mod 2>/dev/null
+rmmod    dfl_fme_mgr \
+	 dfl_fme_region \
+	 dfl_fme_br \
+	 dfl_afu \
+	 dfl_fme \
+	 dfl_pci \
+	 s10hssi \
+	 dfl_n3000_nios \
+	 dfl_spi_altera \
+	 dfl \
+	 fpga_region \
+	 fpga_mgr \
+	 fpga_bridge \
+	 intel_m10_bmc \
+	 intel_m10_bmc_hwmon \
+	 spi_altera \
+	 intel_s10_phy  2>/dev/null
